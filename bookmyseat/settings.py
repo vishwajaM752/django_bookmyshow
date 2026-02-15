aff18503080516f9bbdc67f12f4210864dbfeb6f
VishwajaM752 <vishwajamandhare211@gmail.com>
2026-02-15 20:58:04 +0530

    final workings

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
 
